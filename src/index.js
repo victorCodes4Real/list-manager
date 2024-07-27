8fd7aa59d85df6939c23cee239c5bef308d222d7
victorCodes4Real <achilonuvictor2014@gmail.com>
2024-07-27 23:10:46 +0100

    first-commit

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import { createRoot } from 'react-dom/client';
+
+const domNode = document.getElementById('root');
+const root = createRoot(domNode);
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
